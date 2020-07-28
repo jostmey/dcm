71919e76b80eef8c4b6c3c08b3a95f9faaf556c3
Jared Ostmeyer <jostmey@gmail.com>
2020-07-28 00:55:45 -0500

    Code to build db of TCRs labelled by category. Code to db to CSV files

---
+++ dataset/build_db_category.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+##########################################################################################
+# Author: Jared L. Ostmeyer
+# Date Started: 2020-02-29
+# Purpose: Build database of T-cell receptors labelled by peptide category
+##########################################################################################
+
+##########################################################################################
+# Libraries
+##########################################################################################
+
+import json
+import random
+
+##########################################################################################
+# Settings
+##########################################################################################
+
+path_db_gen = 'db_sequence.json'
+path_db_class = 'db_category.json'
+
+classification_split = [ 0.6, 0.2, 0.2 ]
+min_count = 100
+
+##########################################################################################
+# Load
+##########################################################################################
+
+with open(path_db_gen, 'r') as stream:
+  db_gen = json.load(stream)
+
+##########################################################################################
+# Split samples into a training, validation, and test cohort
+##########################################################################################
+
+db_class = {
+  'train': {},
+  'validate': {},
+  'test': {},
+  'all': {}
+}
+
+for split_gen in [ 'train', 'validate' ]:
+  for peptide, cdr3s in db_gen[split_gen].items():
+    if len(cdr3s) >= min_count:
+      cdr3s_list = list(cdr3s)
+      random.shuffle(cdr3s_list)
+      cumulative_split = 0.0
+      splits = [ 0 ]
+      for split in classification_split:
+        cumulative_split += split
+        splits.append(
+          int(cumulative_split*len(cdr3s))
+        )
+      db_class['train'][peptide] = []
+      for i in range(splits[0], splits[1]):
+        db_class['train'][peptide].append(cdr3s_list[i])
+      db_class['validate'][peptide] = []
+      for i in range(splits[1], splits[2]):
+        db_class['validate'][peptide].append(cdr3s_list[i])
+      db_class['test'][peptide] = []
+      for i in range(splits[2], splits[3]):
+        db_class['test'][peptide].append(cdr3s_list[i])
+      db_class['all'][peptide] = []
+      for i in range(splits[0], splits[3]):
+        db_class['all'][peptide].append(cdr3s_list[i])
+
+##########################################################################################
+# Save the database
+##########################################################################################
+
+with open(path_db_class, 'w') as stream:
+  json.dump(db_class, stream, sort_keys=True)
+
---
+++ dataset/convert_json_to_csv.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+##########################################################################################
+# Author: Jared L. Ostmeyer
+# Date Started: 2020-02-29
+# Purpose: Convert json database to CSV files of each dataset
+##########################################################################################
+
+##########################################################################################
+# Libraries
+##########################################################################################
+
+import argparse
+import json
+
+##########################################################################################
+# Settings
+##########################################################################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--db', help='Path to the json database', type=str, required=True)
+parser.add_argument('--output', help='Basename for CSV output files', type=str, required=True)
+args = parser.parse_args()
+
+##########################################################################################
+# Load database
+##########################################################################################
+
+with open(args.db, 'r') as stream:
+  db = json.load(stream)
+
+##########################################################################################
+# Save datasets
+##########################################################################################
+
+for split in [ 'train', 'validate', 'test', 'all' ]:
+  with open(args.output+'_'+split+'.csv', 'w') as stream:
+    print(
+      'CDR3', 'Vgene', 'Jgene', 'Peptide', 'Frequency', 'Experiment',
+      sep=',', file=stream
+    )
+    num_peptides = len(db[split])
+    for peptide, cdr3s in db[split].items():
+      num_cdr3s = len(cdr3s)
+      for cdr3, entries in cdr3s.items():
+        peptide_ = ':'.join(peptide.split(','))
+        num_entries = len(entries)
+        for entry in entries:
+          frequency = 1.0/(num_peptides*num_cdr3s*num_entries)
+          print(
+            cdr3, entry['vgene'], entry['jgene'], peptide_, frequency, entry['experiment'],
+            sep=',', file=stream
+          )
