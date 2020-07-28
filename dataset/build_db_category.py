79b213fc979f4a177b87de64c194e446b6689f9d
Jared Ostmeyer <jostmey@gmail.com>
2020-07-28 02:30:19 -0500

    Bug fix

--- dataset/build_db_category.py
+++ dataset/build_db_category.py
@@ -20,7 +20,9 @@ path_db_gen = 'db_sequence.json'
 path_db_class = 'db_category.json'
 
 classification_split = [ 0.6, 0.2, 0.2 ]
-min_count = 100
+min_count = 5
+
+random.seed(7424103)
 
 ##########################################################################################
 # Load
@@ -43,7 +45,7 @@ db_class = {
 for split_gen in [ 'train', 'validate' ]:
   for peptide, cdr3s in db_gen[split_gen].items():
     if len(cdr3s) >= min_count:
-      cdr3s_list = list(cdr3s)
+      cdr3s_list = sorted(list(cdr3s))
       random.shuffle(cdr3s_list)
       cumulative_split = 0.0
       splits = [ 0 ]
@@ -54,16 +56,20 @@ for split_gen in [ 'train', 'validate' ]:
         )
       db_class['train'][peptide] = []
       for i in range(splits[0], splits[1]):
-        db_class['train'][peptide].append(cdr3s_list[i])
+        cdr3 = cdr3s_list[i]
+        db_class['train'][peptide].append(cdr3s[cdr3])
       db_class['validate'][peptide] = []
       for i in range(splits[1], splits[2]):
-        db_class['validate'][peptide].append(cdr3s_list[i])
+        cdr3 = cdr3s_list[i]
+        db_class['validate'][peptide].append(cdr3s[cdr3])
       db_class['test'][peptide] = []
       for i in range(splits[2], splits[3]):
-        db_class['test'][peptide].append(cdr3s_list[i])
+        cdr3 = cdr3s_list[i]
+        db_class['test'][peptide].append(cdr3s[cdr3])
       db_class['all'][peptide] = []
       for i in range(splits[0], splits[3]):
-        db_class['all'][peptide].append(cdr3s_list[i])
+        cdr3 = cdr3s_list[i]
+        db_class['all'][peptide].append(cdr3s[cdr3])
 
 ##########################################################################################
 # Save the database
